b455c85b1152312559f6933a4adb018f9ed9e162
ClarkAulden <dqresnme@gmail.com>
2012-04-18 09:35:51 -0400

    Lesson 2 homework

---
+++ Lesson 2/MUS.js
@@ -0,0 +1,44 @@
+function MUS() {
+    /*endtime with par in mind*/
+    this.endTime = function (time, expr) {
+        // your code here
+        if(expr.tag === 'note')
+        {return time + expr.dur;}
+
+        if(expr.tag === 'par')
+        {
+            var durL, durR;
+            durL = arguments.callee(time, expr.left);
+            durR = arguments.callee(time, expr.right);
+            return (durL > durR)? durL : durR;
+        }else
+        {
+            return arguments.callee(time, expr.left) + arguments.callee(time, expr.right);
+        }
+    };
+
+    /*par compile*/
+    this.compile = function (musexpr) {
+        // your code here
+
+        var noteArr = function(musexpr, startTime){
+            if(musexpr.tag === 'note')
+            {
+                musexpr.start = startTime; console.log(musexpr + "note");
+               return [].concat(musexpr);
+            }else
+            {
+                if(musexpr.tag ==='par'){
+                    console.log(musexpr); console.log(" starttime: " + startTime + " par "); 
+                     return [].concat(arguments.callee(musexpr.left, startTime)).concat(arguments.callee(musexpr.right, startTime));
+                }else{
+                    console.log(musexpr); console.log(" starttime: " + startTime + " seq");
+                    return [].concat(arguments.callee(musexpr.left, startTime)).concat(arguments.callee(musexpr.right, endTime(startTime, musexpr.left)));
+                }
+            }
+        }(musexpr, 0);
+
+        return noteArr;
+    };
+
+}
