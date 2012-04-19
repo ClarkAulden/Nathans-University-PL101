636931383e7c3c10b6e52debf93113d7a56fad69
ClarkAulden <dqresnme@gmail.com>
2012-04-19 14:59:47 -0400

    rests and repeats added player needs work

---
+++ MUS/compileTest.js
@@ -0,0 +1,16 @@
+var MUS = require("./MUS.js");
+
+var musHelper = new MUS.MUS();
+var melody_mus = 
+    { tag: 'seq',
+      left: 
+       { tag: 'seq',
+         left: { tag: 'note', pitch: 'a4', dur: 250 },
+         right: { tag: 'note', pitch: 'b4', dur: 250 } },
+      right:
+       { tag: 'seq',
+         left: { tag: 'note', pitch: 'c4', dur: 500 },
+         right: { tag: 'note', pitch: 'd4', dur: 500 } } };
+
+console.log(melody_mus);
+console.log(musHelper.compile(melody_mus));
